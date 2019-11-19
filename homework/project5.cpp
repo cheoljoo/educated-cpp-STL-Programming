d43546b79474d266ea26ea484fcd267987c32682
Charles.Lee <cheoljoo@gmail.com>
2019-11-19 21:08:41 +0900

    #5

--- homework/project5.cpp
+++ homework/project5.cpp
@@ -1,5 +1,24 @@
-// 6-4강에서는 NotificationCenter를 만들었습니다. 동영상 강좌에서 만든 코드는 통보센터에 등록된 순서대로 함수가 호출 됩니다.
-// NotificationCenter 코드를 수정해서 함수 등록시 우선순위를 전달할 수 있고, 멀티 스레드가 지원되도록 수정해 보세요.
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <functional>
+#include <map>
+#include <vector>
+#include <chrono>
+#include <thread>
+using namespace std;
+using namespace std::placeholders;
+
+
+#define NEW_THREAD 1
+
+using HANDLER = function<void(void*)>;
+
+bool sortByPriority(const tuple<int, int, HANDLER>& a,
+               const tuple<int, int, HANDLER>& b)
+{
+    return (get<0>(a) > get<0>(b));
+}
 
 void foo(void* p, string name)
 {
@@ -7,6 +26,37 @@ void foo(void* p, string name)
     this_thread::sleep_for(3s);
     cout << name << " end" << endl;
 }
+
+class NotificationCenter
+{
+    map<string, vector<tuple<int,int,HANDLER>>> notif_map;
+public:
+    void Register(string key, HANDLER h,int priority,int thd=0)
+    {
+        notif_map[key].push_back(make_tuple(priority,thd,h));
+    }
+    void Notify( string key, void* param)
+    {
+        vector<tuple<int,int,HANDLER>>& v = notif_map[key];
+
+        for( tuple<int,int,HANDLER> f : v){
+            cout << "priority:" << get<0>(f) <<  " , thread:" << get<1>(f) << endl;
+            if(get<1>(f) == NEW_THREAD){
+                thread t(get<2>(f),param);
+                        //thread t(&foo,param,"PPP");
+                t.join();
+            } else {
+                get<2>(f)(param);
+            }
+        }
+    }
+    void Sort(string key)
+    {
+        vector<tuple<int,int,HANDLER>>& v = notif_map[key];
+        sort(v.begin(),v.end(),sortByPriority);
+    }
+};
+
 int main()
 {
     NotificationCenter nc;
@@ -18,6 +68,7 @@ int main()
     nc.Register("TEST_EVENT", bind(&foo, _1, "two"s), 2, NEW_THREAD);
     nc.Register("TEST_EVENT", bind(&foo, _1, "three"s), 3);
 
+    nc.Sort("TEST_EVENT");
     nc.Notify("TEST_EVENT", (void*)0); // 등록된 순서가 아닌 우선순위가
                 // 높은 순서로 실행되게 해주세요
                 // NEW_THREAD 옵션을 가진 경우. 새로운
